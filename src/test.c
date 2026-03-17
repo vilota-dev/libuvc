6c90a7b454a1c2e2173bbb60f3fd224594c40526
huimin.c <huimin@vilota.ai>
2026-03-17 19:47:03 +0800

    modification to test.c

--- src/test.c
+++ src/test.c
@@ -32,42 +32,79 @@
 *  POSSIBILITY OF SUCH DAMAGE.
 *********************************************************************/
 #include <stdio.h>
-#include <opencv2/highgui/highgui_c.h>
 
 #include "libuvc/libuvc.h"
+#include <stdint.h>
+#include <sys/time.h>
+
 
 void cb(uvc_frame_t *frame, void *ptr) {
   uvc_frame_t *bgr;
-  uvc_error_t ret;
-  IplImage* cvImg;
 
-  printf("callback! length = %u, ptr = %d\n", frame->data_bytes, (int) ptr);
+  static uint64_t total_frames = 0;
+  static uint64_t window_frames = 0;
+  static int initialized = 0;
+  static struct timeval start_tv;
+  static struct timeval last_tv;
+
+  struct timeval now;
+  double total_elapsed;
+  double window_elapsed;
+  double fps_avg;
+  double fps_inst;
+
+  if (!initialized) {
+    gettimeofday(&start_tv, NULL);
+    last_tv = start_tv;
+    initialized = 1;
+    return;
+  }
+
+  gettimeofday(&now, NULL);
+
+  total_frames++;
+  window_frames++;
+
+  total_elapsed =
+      (double)(now.tv_sec - start_tv.tv_sec) +
+      (double)(now.tv_usec - start_tv.tv_usec) / 1000000.0;
+
+  window_elapsed =
+      (double)(now.tv_sec - last_tv.tv_sec) +
+      (double)(now.tv_usec - last_tv.tv_usec) / 1000000.0;
+
+  fps_avg = (total_elapsed > 0.0) ? ((double)total_frames / total_elapsed) : 0.0;
+  fps_inst = (window_elapsed > 0.0) ? ((double)window_frames / window_elapsed) : 0.0;
+
+  printf("%llu: callback! length = %u, ptr = %p\n",
+         (unsigned long long)total_frames,
+         (unsigned)frame->data_bytes,
+         ptr);
+  printf("width %u, height %u\n",
+         (unsigned)frame->width,
+         (unsigned)frame->height);
+
+  // Print FPS once per second to avoid spamming too much
+  if (window_elapsed >= 1.0) {
+    printf("FPS(inst)=%.2f, FPS(avg)=%.2f\n", fps_inst, fps_avg);
+    last_tv = now;
+    window_frames = 0;
+  }
 
   bgr = uvc_allocate_frame(frame->width * frame->height * 3);
   if (!bgr) {
-    printf("unable to allocate bgr frame!");
+    printf("unable to allocate bgr frame!\n");
     return;
   }
 
-  ret = uvc_any2bgr(frame, bgr);
-  if (ret) {
-    uvc_perror(ret, "uvc_any2bgr");
-    uvc_free_frame(bgr);
-    return;
-  }
+  // ret = uvc_any2bgr(frame, bgr);
+  // if (ret) {
+  //   uvc_perror(ret, "uvc_any2bgr");
+  //   uvc_free_frame(bgr);
+  //   return;
+  // }
 
-  cvImg = cvCreateImageHeader(
-      cvSize(bgr->width, bgr->height),
-      IPL_DEPTH_8U,
-      3);
-
-  cvSetData(cvImg, bgr->data, bgr->width * 3); 
-
-  cvNamedWindow("Test", CV_WINDOW_AUTOSIZE);
-  cvShowImage("Test", cvImg);
-  cvWaitKey(10);
-
-  cvReleaseImageHeader(&cvImg);
+  // cvWaitKey(10);
 
   uvc_free_frame(bgr);
 }
@@ -90,7 +127,10 @@ int main(int argc, char **argv) {
 
   res = uvc_find_device(
       ctx, &dev,
-      0, 0, NULL);
+      16981, //vid
+      24065, //pid
+      NULL);
+
 
   if (res < 0) {
     uvc_perror(res, "uvc_find_device");
@@ -107,7 +147,7 @@ int main(int argc, char **argv) {
       uvc_print_diag(devh, stderr);
 
       res = uvc_get_stream_ctrl_format_size(
-          devh, &ctrl, UVC_FRAME_FORMAT_YUYV, 640, 480, 30
+          devh, &ctrl, UVC_FRAME_FORMAT_MJPEG, 1920, 1080, 30
       );
 
       uvc_print_stream_ctrl(&ctrl, stderr);
@@ -121,14 +161,14 @@ int main(int argc, char **argv) {
           uvc_perror(res, "start_streaming");
         } else {
           puts("Streaming for 10 seconds...");
-          uvc_error_t resAEMODE = uvc_set_ae_mode(devh, 1);
-          uvc_perror(resAEMODE, "set_ae_mode");
+          // uvc_error_t resAEMODE = uvc_set_ae_mode(devh, 1);
+          // uvc_perror(resAEMODE, "set_ae_mode");
           int i;
           for (i = 1; i <= 10; i++) {
             /* uvc_error_t resPT = uvc_set_pantilt_abs(devh, i * 20 * 3600, 0); */
             /* uvc_perror(resPT, "set_pt_abs"); */
-            uvc_error_t resEXP = uvc_set_exposure_abs(devh, 20 + i * 5);
-            uvc_perror(resEXP, "set_exp_abs");
+            // uvc_error_t resEXP = uvc_set_exposure_abs(devh, 20 + i * 5);
+            // uvc_perror(resEXP, "set_exp_abs");
             
             sleep(1);
           }
